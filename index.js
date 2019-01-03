3e90f5d0181b5854c0019f1db139736d47af1126
zaid85j <zaid85j@gmail.com>
2019-01-03 10:59:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ const logDriversByHometown = function (drivers, hometown) {
 				
 		var totalRevenue = function (drivers) {
   return drivers.reduce(function (sumation, thedriver) {
-    return thedriver.income + sumation});
+    return thedriver. revenue  + sumation});
 };
 				
