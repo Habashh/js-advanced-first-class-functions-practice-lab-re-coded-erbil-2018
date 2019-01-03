e6accf52cedaec7df59e1ae7a29147a413ea161a
zaid85j <zaid85j@gmail.com>
2019-01-03 11:02:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ const logDriversByHometown = function (drivers, hometown) {
     return firstDriver.name.localeCompare(secondDriver.name); });
 				};
 				
-		var totalRevenue = function (drivers) {
-  return drivers.reduce(function (sumation, thedriver) {
+		var totalRevenue = function (driver) {
+  return driver.reduce(function (sumation, thedriver) {
     return thedriver. revenue  + sumation},0);
 };
 				
